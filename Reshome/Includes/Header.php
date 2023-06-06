dc7f3ea6c7adb1fc64dae5e41fa6175a30801da9
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-06-06 14:26:45 +0200

    Ajout de la navbar et du header

---
+++ Reshome/Includes/Header.php
@@ -0,0 +1,15 @@
+<div class="container-header">
+    <img class="img-background" src="../CSS/Assets/backgrounds.png" alt="">
+
+    <h1 class="header-title">
+        VICTORY PARK <span class="header-title-color">Residence</span>
+    </h1>
+
+    <div class="container-header-info">
+        <img src="../CSS/Assets/img-info-header.png" alt="">
+        <div class="header-info">
+            <p class="header-info-text-color">LUXURY RESIDENCE</p>
+            <P>+33 7 80 10 23 51</P>
+        </div>
+    </div>
+</div>
---
+++ Reshome/index.php
@@ -0,0 +1,18 @@
+<?php 
+require('actions/users/securityAction.php');
+require('actions/posts/ShowAllPostsAction.php');
+?>
+<!DOCTYPE html>
+<html lang="fr"> 
+<?php include 'includes/head.php'; ?>
+<body>
+    <?php include 'includes/navbar.php'; ?>
+    <?php include 'includes/Header.php'; ?>
+    <div class="container-index">
+    
+    </div>
+
+
+<script src="../JS/index.js"></script>    
+</body>
+</html>
