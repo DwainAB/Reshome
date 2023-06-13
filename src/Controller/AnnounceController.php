61d257aff1cc57e70475355b18050c7c7e0bd7e1
Léo <bacquartl@icloud.com>
2023-06-13 15:53:54 +0200

    Controller Announce

--- src/Controller/AnnounceController.php
+++ src/Controller/AnnounceController.php
@@ -2,6 +2,7 @@
 
 namespace Hetic\ReshomeH\Controller;
 
+use Hetic\ReshomeH\Model\Class\Announce;
 use Hetic\ReshomeH\Model\Manager\AnnounceManager;
 
 class AnnounceController
@@ -12,8 +13,21 @@ class AnnounceController
     {
         $manager = new AnnounceManager();
         $announces = $manager->getAllAnnounces();
-        var_dump($announces);
+        $this->render('Annonces', $announces);
     }
+
+    public function render(string $title, array $announces) : mixed
+    {
+        $viewDir = dirname(__DIR__, 1) . '/View';
+        $template = $viewDir . '/Templates/skeleton.php';
+        $view = $viewDir . '/announce.php';
+
+        ob_start();
+        require $view;
+        $content = ob_get_clean();
+        return require $template;
+    }
+
 
 
 }
---
+++ src/View/announce.php
@@ -0,0 +1,7 @@
+<?php foreach ($announces as $announce): ?>
+<div class="announce">
+    <h2><?= $announce->getTitle() ?></h2>
+
+</div>
+
+<?php endforeach; ?>
