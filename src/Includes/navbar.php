06a653f742ea1e434b1ba817244ebb363ba6d805
Vidocq17 <AvidanB1704@gmail.com>
2023-06-16 11:07:04 +0200

    add icons

--- src/Includes/navbar.php
+++ src/Includes/navbar.php
@@ -1,16 +1,37 @@
+
+<div class="navbar-menu">
+    <img src=".Reshome/public/CSS/Assets/menu.svg" id="btn-menu">
+</div>
+<img class="img-nav2" src="./public/CSS/Assets/logo.svg" alt="" id="img-menu">
+
+
 <div class="container-navbar">
+
         <ul class="info-left">
-            <li class="info">ABOUT PROJECT</li>
-            <li class="info">CONSTRUCTION PROGRESS</li>
+            <li class="info">ABOUT</li>
+            <li class="info">APPARTMENT</li>
             <li class="info">CONTACT</li>
         </ul>
 
         <img class="img-nav" src="./public/CSS/Assets/logo.svg" alt="">
 
         <ul class="info-right">
-            <li class="info">+33 6 32 01 01 93</li>
-            <li class="info li-border">SEARCH APPARTEMENT  <span class="img-li-border"><img src="../CSS/Assets/arrowdown.png" alt=""></span></li>
-            <li class="info">AVIS</li>
+            <li class="info li-border">SEARCH<span class="img-li-border"><img src="../CSS/Assets/arrowdown.png" alt=""></span></li>
+            <li class="info">LOGIN</li>
+            <li class="info">REGISTER</li>
         </ul>
 </div>
 
+
+<script>
+  //  const menuBtn = document.getElementById('btn-menu');
+  const menuImg = document.getElementById('img-menu');
+    const navbar = document.getElementById('.container-navbar');
+
+   // menuBtn.addEventListener('click', function() {
+    //    navbar.classList.toggle('open');
+    // });
+    menuImg.addEventListener('click', function() {
+      navbar.classList.toggle('open');
+    });
+</script>
