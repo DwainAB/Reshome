f4a06e9ff25626bc1dee5e648f802f79c3ae1811
Vidocq17 <AvidanB1704@gmail.com>
2023-06-16 11:57:35 +0200

    navbar

---
+++ public/JS/navbar.js
@@ -0,0 +1,13 @@
+// Bouton navbar
+
+  //  const menuBtn = document.getElementById('btn-menu');
+  const menuImg = document.getElementById('img-menu');
+    const navbar = document.getElementById('.container-navbar');
+
+   // menuBtn.addEventListener('click', function() {
+    //    navbar.classList.toggle('open');
+    // });
+    menuImg.addEventListener('click', function() {
+      navbar.
+      navbar.classList.toggle('open');
+    });
--- src/Includes/navbar.php
+++ src/Includes/navbar.php
@@ -23,16 +23,3 @@
 </div>
 
 
-<script>
-  //  const menuBtn = document.getElementById('btn-menu');
-  const menuImg = document.getElementById('img-menu');
-    const navbar = document.getElementById('.container-navbar');
-
-   // menuBtn.addEventListener('click', function() {
-    //    navbar.classList.toggle('open');
-    // });
-    menuImg.addEventListener('click', function() {
-      navbar.
-      navbar.classList.toggle('open');
-    });
-</script>
