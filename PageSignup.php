f009fa2b9436cfad11ba3f5b8aa590de508f984e
Aghiles Haddar <haddaraghiles5@gmail.com>
2023-06-21 01:23:54 +0200

    login finish

--- PageSignup.php
+++ PageSignup.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr"> 
-<?php include './src/includes/HeadRegister.php'; ?>
+<?php include './src/includes/HeadSignup.php'; ?>
 <body>
     <div class="container-pageAppart">
         <?php include './src/includes/navbar.php'; ?>
-        <?php include './src/includes/register.php'; ?>
+        <?php include './src/includes/signup.php'; ?>
         <?php include './src/includes/Footer.php'; ?>
     </div>
 
-<script src="./public/JS/CallApi.js"></script>
-<script src="./public/JS/Register.js"></script>    
+<script src="./public/JS/CallAp.js"></script>
+<script src="./public/JS/singup.js"></script>    
 </body>
 </html>
---
+++ src/Includes/signup.php
@@ -0,0 +1,47 @@
+<div class="register-container-g" >
+
+    <div class="register-right-part" >
+
+            <div class="register-right-small-part" >
+
+                <img  class="register-right-logo"src="./public/CSS/Assets/logo.svg" alt="">
+
+                <h2 class="register-right-title" >Reshome</h2>
+
+                <h2 class="register-right-text" >Your luxury real estate <br> rental platform in Paris</h2>
+
+            </div>
+        
+    </div>
+
+    <div class="register-left-part" >
+
+        <img class="register-left-logo" src="./public/CSS/Assets/logo.svg" alt="">
+        
+        <h3 class="register-left-title" >Hey, Hello</h3>
+
+        <p class="register-left-text" >Enter your first information for your first connection </p>
+
+        <form class="register-left-form" action="http://localhost:8080/api/auth/login" method="GET">
+
+            <div class="email-g container-input-register-left">
+                <label class="register-left-label" for="">Email</label>
+                <input class="register-left-input" name="email" type="email">
+            </div>
+
+            <div class="password-g container-input-register-left">
+                <label class="register-left-label" for="">Password</label>
+                <input class="register-left-input"  name="password" type="password">
+            </div>
+
+            <div class="register-left-div-btn" >
+                <button class="register-left-btn" type="submit" >Login</button>
+            </div>
+
+        </form>
+
+    </div>
+
+    
+
+</div>
