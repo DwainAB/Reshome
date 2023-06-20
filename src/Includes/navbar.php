58ebae85d62b70e2abeec041a808a7db8ee41e02
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-06-20 19:53:45 +0200

    ajout des images

--- src/Includes/navbar.php
+++ src/Includes/navbar.php
@@ -1,16 +1,16 @@
 <div class="container-navbar">
         <ul class="info-left">
-            <li class="info">ABOUT PROJECT</li>
-            <li class="info">CONSTRUCTION PROGRESS</li>
-            <li class="info">CONTACT</li>
+            <li class="info">Home</li>
+            <li class="info">Apartment</li>
+            <li class="info">Contact</li>
         </ul>
 
         <img class="img-nav" src="./public/CSS/Assets/logo.svg" alt="">
 
         <ul class="info-right">
-            <li class="info">+33 6 32 01 01 93</li>
-            <li class="info li-border">SEARCH APPARTEMENT  <span class="img-li-border"><img src="../CSS/Assets/arrowdown.png" alt=""></span></li>
-            <li class="info">AVIS</li>
+            <li class="info"></li>
+            <li class="info li-border">Start a search  <span class="img-li-border"><img src="../CSS/Assets/arrowdown.png" alt=""></span></li>
+            <li class="info">Login</li>
         </ul>
 </div>
 
