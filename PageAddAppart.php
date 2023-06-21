f3facff8743d4afedbfd13f70d23c589a729bb60
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-06-21 23:12:03 +0200

    ajout de la page pour ajouter des appart

---
+++ PageAddAppart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include './src/includes/HeadAdmin.php'; ?>
+
+<body>
+    <div class="container-pageAppart">
+        <?php include './src/includes/ContentAddAppart.php'; ?>
+    </div>
+
+
+    <script src="./public/JS/CallApi.js"></script>
+    <script src="./public/JS/PageAddAppart.js"></script>
+</body>
+
+</html>
